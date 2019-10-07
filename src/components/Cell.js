18b8f4cafb99e67e63cde3d2261021bc818e82bf
Adriana <adrianacucu99@gmail.com>
2019-10-07 18:20:16 +0100

    Removed text from cells

--- src/components/Cell.js
+++ src/components/Cell.js
@@ -4,9 +4,7 @@ import { StyledCell } from './styles/StyledCell';
 import { TETROMINOS } from '../tetrominos';
 
 const Cell = ({ type }) => (
-  <StyledCell type={type} color={TETROMINOS[type].color}>
-    cell
-  </StyledCell>
+  <StyledCell type={type} color={TETROMINOS[type].color} />
 );
 
 export default Cell;
