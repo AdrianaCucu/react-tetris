cf7555449eed3d5c3ea40acc22ab2af9307ff844
Adriana <adrianacucu99@gmail.com>
2019-10-07 17:33:34 +0100

    Added I-shaped cell

--- src/tetrominos.js
+++ src/tetrominos.js
@@ -1,3 +1,7 @@
 export const TETROMINOS = {
-  0: { shape: [[0]], colour: '0, 0, 0' }
+  0: { shape: [[0]], colour: '0, 0, 0' },
+  I: {
+    shape: [[0, 'I', 0, 0], [0, 'I', 0, 0], [0, 'I', 0, 0], [0, 'I', 0, 0]],
+    colour: '80, 227, 230'
+  }
 };
