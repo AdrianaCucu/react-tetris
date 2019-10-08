360f29c78d8cb99deb6d7c057b54be17185f8e2e
Adriana <adrianacucu99@gmail.com>
2019-10-08 14:00:06 +0100

    Fixed typo for left right collision

--- src/gameHelpers.js
+++ src/gameHelpers.js
@@ -7,7 +7,7 @@ export const createStage = () =>
   );
 
 export const checkCollision = (player, stage, { x: moveX, y: moveY }) => {
-  for (let y = 0; y < player.tetromino.lemgth; y += 1) {
+  for (let y = 0; y < player.tetromino.length; y += 1) {
     for (let x = 0; x < player.tetromino[0].length; x += 1) {
       // 1. Check that we're on an actual Tetromino cell.
       if (player.tetromino[y][x] !== 0) {
